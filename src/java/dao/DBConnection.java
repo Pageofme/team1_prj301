153305e7c9de15ab5d10c0f9056f9649cb18e56e
nkmq21 <ndduy269@gmail.com>
2025-03-02 17:23:19 +0700

    spine

---
+++ src/java/dao/DBConnection.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class DBConnection
+{
+    public static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static final String URL = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=LigmaShop;encrypt=false;trustServerCertificate=false";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "123456";   
+        public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName(driverName);
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            return con;
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(DBConnection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        try(Connection con = getConnection()) {
+            if (con != null) {
+                System.out.println("connect success");
+            }
+        } catch (SQLException ex) {
+            java.util.logging.Logger.getLogger(DBConnection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+}
